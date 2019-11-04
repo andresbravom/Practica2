152358ef81c7cd1a68b3d641c1bc6398048617ee
Andrés Bravo <andresbravomontes89@gmail.com>
2019-11-04 13:54:39 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,102 +1,54 @@
-import { GraphQLServer } from "graphql-yoga";
-import { getFilmsInfo } from "./utils";
-import { fetchData } from "./fetchData";
+import  {GraphQLServer} from 'graphql-yoga'
+import * as uuid from 'uuid'
 
-const runApp = function(peopleData, filmsData) {
-  const typeDefs = `
-    type Query {
-      people(page: Int, number: Int, name: String, gender: String): PeopleReturn!
-      character(id: Int!):Character
-    }
+const authorData = [{
+  name: String,
+  email: String,
+  listaRecetas: [recetas],
+  id: "ksvopvkspaokvopkv"
+}];
 
-    type PeopleReturn {
-      page: Int!
-      totalPages: Int!
-      totalPeople: Int!
-      people: [Character!]
-    }
+const recetas = [{
+  id: uuid,
+  titulo: String,
+  descripcion: String,
+  fechaIntroduccion: 122345,
+  autor: "amskadkjakpdja",
+  ingredientes: [ingredientes]
+}];
 
-    type Character {
-      name: String!
-      gender: String!
-      url: String!
-      id: Int!
-      films: [Film!]
-    }
+const ingredientes = [{
+  nombre: String,
+  recetasQueAparecen
+}];
 
-    type Film {
-      title: String!
-      episode: String!
-      url: String!
-    }
-  `;
+const typeDefs = `
+  type Author{
+    name: STring!
+    email: String!
+    listaRecetas[Recetas!]!
+    id: ID!
+  }
 
-  const resolvers = {
-    Query: {
-      people: (parent, args, ctx, info) => {
-        const page = args.page || 1;
-        const number = args.number || 10;
+  type Recetas{
+    titulo: String!
+    descripcion: String!
+    fechaIntroduccion: Int!
+    autor: Author!
+    ingredientes: [Ingredientes]!
+    id = ID!
+  }
 
-        const init = (page - 1) * number;
-        const end = init + number;
+  type Ingredientes{
+    nombre: String!
+    recetasQueAparecen: [Recetas]
+  }
 
-        let result = peopleData.slice();
+  type Query{
 
-        if (args.name) {
-          result = result.filter(obj =>
-            obj.name.toUpperCase().includes(args.name.toUpperCase())
-          );
-        }
+  }
 
-        if (args.gender) {
-          result = result.filter(
-            obj => obj.gender.toUpperCase() === args.gender.toUpperCase()
-          );
-        }
-
-        const people = result.slice(init, end).map((character, index) => {
-          return {
-            name: character.name,
-            gender: character.gender,
-            url: character.url,
-            id: index,
-            films: getFilmsInfo(character.films, filmsData)
-          };
-        });
-
-        return {
-          totalPeople: peopleData.length,
-          totalPages: Math.ceil(peopleData.length / number),
-          page,
-          people
-        };
-      },
-
-      character: (parent, args, ctx, info) => {
-        const char = peopleData[args.id];
-        return {
-          name: char.name,
-          gender: char.gender,
-          url: char.url,
-          id: args.id,
-          films: getFilmsInfo(char.films, filmsData)
-        };
-      }
-    }
-  };
-
-  const server = new GraphQLServer({ typeDefs, resolvers });
-
-  const options = {
-    port: 4000
-  };
-
-  server.start(options, ({ port }) =>
-    console.log(
-      `Server started, listening on port ${port} for incoming requests.`
-    )
-  );
-};
-
-// Fetch Data and then run app
-fetchData(runApp);
+  type Mutation{
+    
+  }
+`
